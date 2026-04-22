a02f3ca589f4d8fe8fe21f4b61ff0b7c867e9dba
ArkoseLabs <126958051+ArkoseLabsOfficial@users.noreply.github.com>
2026-04-23 00:32:55 +0300

    Update FileDialog.h

--- project/include/ui/FileDialog.h
+++ project/include/ui/FileDialog.h
@@ -1,11 +1,9 @@
 #ifndef LIME_UI_FILE_DIALOG_H
 #define LIME_UI_FILE_DIALOG_H
 
-
 #include <string>
 #include <vector>
 #include <ui/FileDialogEvent.h>
-
 
 #ifdef IPHONE
 #ifdef __OBJC__
@@ -14,31 +12,32 @@
 typedef struct objc_object FileDialogObserver;
 #endif
 #endif
+
 namespace lime {
 
+    class FileDialog {
 
-	class FileDialog {
-
-		public:
-			#ifdef IPHONE
-			static int Create();
-			static void Open (int id_handle);
-			static void BrowseSelect (int id_handle);
-			static void BrowseSelectMultiple (int id_handle);
-			static void LaunchFileDialogOpen(FileDialogObserver *observer, bool selectMultiple);
-			// static std::wstring* OpenDirectory (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
-			// static void OpenFiles (std::vector<std::wstring*>* files, std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
-			// static std::wstring* SaveFile (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
-			#else
-			static std::wstring* OpenDirectory (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
-			static std::wstring* OpenFile (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
-			static void OpenFiles (std::vector<std::wstring*>* files, std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
-			static std::wstring* SaveFile (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
-			#endif
-	};
-
+        public:
+            #ifdef IPHONE
+            static int Create();
+            static void Open (int id_handle);
+            static void BrowseSelect (int id_handle);
+            static void BrowseSelectMultiple (int id_handle);
+            static void Save (int id_handle, const char* path);
+            static void BrowseSave (int id_handle, const char* path);
+            static void LaunchFileDialogOpen(FileDialogObserver *observer, bool selectMultiple);
+            static void LaunchFileDialogSave(FileDialogObserver *observer, const char* path);
+            // static std::wstring* OpenDirectory (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
+            // static void OpenFiles (std::vector<std::wstring*>* files, std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
+            // static std::wstring* SaveFile (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
+            #else
+            static std::wstring* OpenDirectory (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
+            static std::wstring* OpenFile (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
+            static void OpenFiles (std::vector<std::wstring*>* files, std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
+            static std::wstring* SaveFile (std::wstring* title = 0, std::wstring* filter = 0, std::wstring* defaultPath = 0);
+            #endif
+    };
 
 }
-
 
 #endif
